f343072c1aac395153ddc60b2b4cd08a8cce4e48
SkorihV <skorihv@mail.ru>
2022-10-30 22:43:34 +0700

    Первый залив

---
+++ src/js/import/components.js
@@ -0,0 +1,6 @@
+import "%components%/header/header.js";
+import "%components%/aboutSecond/aboutSecond.js";
+import "%components%/productList/productList.js";
+import "%components%/readyProducts/readyProducts.js";
+import "%components%/formBlock/formBlock.js";
+import "%components%/footer/footer.js";
